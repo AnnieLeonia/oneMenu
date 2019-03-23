ebe1801478db44758f8b56e1fc1e3aea73150b9b
annieleonia <annie.onnered@gmail.com>
2019-03-23 14:16:03 +0100

    Add sequelize to server

--- server/index.js
+++ server/index.js
@@ -1,61 +1,96 @@
+const Sequelize = require("sequelize");
 const express = require("express");
-const session = require("cookie-session");
-const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require("path");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const { find } = require("lodash");
+const cookieParser = require("cookie-parser");
+const session = require("cookie-session");
 require("dotenv").config();
 
-const { Client } = require("pg");
-const passport = require("passport");
-
 const app = express();
-const port = 3010;
-const { DATABASE_URL } = process.env;
 
-// serve static react build
 app.use(express.static(path.resolve(__dirname, "..", "dist")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// connect to our database
-// const client = new Client(DATABASE_URL);
-// client.connect();
+const sequelize = new Sequelize(
+  process.env.SEQUELIZE_DB,
+  process.env.SEQUELIZE_USERNAME,
+  process.env.SEQUELIZE_PASSWORD,
+  {
+    host: process.env.SEQUELIZE_HOST,
+    dialect: "postgres",
+    operatorsAliases: false
+  }
+);
 
-// load models
-// const User = require('./models/user')(client);
-// const Category = require('./models/category')(client);
-// const Product = require('./models/product')(client);
+const Chef = sequelize.define("chef", {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  photo: Sequelize.STRING,
+  language: Sequelize.STRING
+});
 
-// const db = { User, Category, Product };
+passport.serializeUser(function(user, done) {
+  done(null, user.name);
+});
 
-// require('./passport')(passport, User);
+passport.deserializeUser(function(name, done) {
+  done(null, { name });
+});
 
-// set up our express application
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(
-  bodyParser.urlencoded({
-    extended: true
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+
+    async function(accessToken, refreshToken, profile, done) {
+      const chefs = await Chef.findAll();
+      const chef = find(chefs, { email: profile.emails[0].value });
+      if (chef) {
+        return done(null, chef);
+      }
+      return done({ success: false, message: "401 Unauthorized" });
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"] // ["https://www.googleapis.com/auth/plus.login"]
   })
 );
-app.use(bodyParser.json());
 
-// required for passport
-// const { SESSION_SECRET_KEY1, SESSION_SECRET_KEY2 } = process.env;
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    res.cookie("name", req.user.name);
+    res.redirect("/");
+  }
+);
 
-// app.use(
-//   session({
-//     name: 'session',
-//     keys: [SESSION_SECRET_KEY1, SESSION_SECRET_KEY2],
-//     // Cookie Options
-//     httpOnly: true,
-//     resave: false,
-//     rolling: true,
-//     saveUninitialized: false,
-//     maxAge: 30 * 24 * 60 * 60 * 1000, // one month
-//   })
-// ); // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
+app.get("/auth/me", (req, res) => res.send(req.user));
 
-// routes
-// require('./routes.js')(app, passport, db);
+app.get("/api", (req, res) => res.send("Hello Server!"));
 
-app.listen(port, () => console.log("OneMenu app listening on port 3010!"));
+app.get("/api/chefs", async (req, res) => {
+  const chefs = await Chef.findAll();
+  res.send(chefs);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
+});
+
+app.listen(3010, () => console.log("App is up and running!"));
