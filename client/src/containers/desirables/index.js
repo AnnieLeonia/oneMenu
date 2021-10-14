c331d06de62306dba11638df38998d55dc4bafb6
Victor Winberg <victor.m.winberg@gmail.com>
2021-10-14 16:25:48 +0200

    Fix select suggestion (#53)

--- client/src/containers/desirables/index.js
+++ client/src/containers/desirables/index.js
@@ -3,14 +3,15 @@ import React from "react";
 import DesiredList from "./DesiredList";
 import New from "../common/New";
 import Snackbar from "../common/Snackbar";
-import { toggleDishInactive, removeDish } from "../../actions/dishes";
+import { addDish, toggleDishInactive, removeDish } from "../../actions/dishes";
 
 const Dishes = (props) => (
   <div>
     <New
       view="dishes"
       autosuggest
-      onAdd={(dish) => toggleDishInactive(dish.id)}
+      onAdd={addDish}
+      onSelect={toggleDishInactive}
       onRemove={removeDish}
       {...props}
     />
