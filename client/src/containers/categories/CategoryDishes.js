abefbac383576fcc89706715debe7f92c2c4d7c0
Victor Winberg <victor.m.winberg@gmail.com>
2024-06-23 18:50:44 +0200

    fix: categoy dishes search (#73)

--- client/src/containers/categories/CategoryDishes.js
+++ client/src/containers/categories/CategoryDishes.js
@@ -4,10 +4,16 @@ import CategoryDishList from "./CategoryDishList";
 import New from "../common/New";
 import Snackbar from "../common/Snackbar";
 import { addCategory } from "../../actions/categories";
+import { searchInput } from "../../actions/search";
 
 const CategoryDishes = (props) => (
   <div>
-    <New view="categories" onAdd={addCategory} {...props} />
+    <New
+      view="categories"
+      onAdd={addCategory}
+      onSearch={searchInput}
+      {...props}
+    />
     <Snackbar />
     <CategoryDishList view="dishes" {...props} />
   </div>
