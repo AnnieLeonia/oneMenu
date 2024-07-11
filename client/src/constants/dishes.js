ef1c769f3bf6e04558437253ff3dc3316a1cdca6
Victor Winberg <victor.m.winberg@gmail.com>
2024-07-11 12:45:41 +0200

    feat(dish): add default description

--- client/src/constants/dishes.js
+++ client/src/constants/dishes.js
@@ -1,2 +1,26 @@
 export const FETCH_DISH = "FETCH_DISH";
 export const FETCH_DISHES = "FETCH_DISHES";
+
+export const DEFAULT_DESCRIPTION = (dish) => `# ${dish.name}
+Svårighetsgrad   | Tid
+---------------- | -------------
+Svår/Medel/Enkel | 15/30/45 min
+
+En kort beskrivning av rätten.
+
+## Ingredienser
+Mängd | Ingrediens
+----- | ----------
+500 g | Kärlek
+500 g | Glädje
+1 st  | Humor
+1/2 st | Tålamod
+&nbsp; | salt
+&nbsp; | peppar
+
+## Tillagning
+1. Dela kärleken.
+2. Blanda glädje och humor.
+3. Tillsätt tålamod.
+4. Salta och peppra efter smak.
+`;
